0834617fdacd5dd2354dbed1241793fe45fa2f2f
milesjrose <s2093547@ed.ac.uk>
2025-02-25 01:08:34 +0000

    debugging, added print token fuction

--- fileHandling.py
+++ fileHandling.py
@@ -38,7 +38,7 @@ def buildNetFromFile(fileName):
         return None
     
     # build net from props
-    sym = bn.interpretSymfile(symProps)[0]
+    sym = bn.interpretSymfile(sym)[0]
     mem = bn.initializeMemorySet()
     mem = bn.buildTheNetwork(sym, mem)
     return mem
@@ -67,84 +67,61 @@ class tensorBuilder(object):
     def formatMemory(self):
         nodes = []                                                      # To store all formatted nodes
         self.identifyNodes()
-        for anumber in range(self.mem.analogs):                         # iterate through analogs, keeping track of analog number
+        for anumber in range(len(self.mem.analogs)):                         # iterate through analogs, keeping track of analog number
             analog: dt.Analog = self.mem.analogs[anumber]               # - for autocomplete
             types = [analog.myPOs, analog.myRBs, analog.myPs, analog.myGroups]
             for tokens in types:                                        
-                for token in tokens: 
-                    # Token                                   
-                    tk = self.formatToken(token, anumber)           
-                    nodes.append(tk)
-                    
-                    # Mappings
-                    addedMaps = {}
-                    for mapHyp in token.mappingHypotheses:          # Add hypotheses
-                        mp = []                                         # emtpy mapping
-                        mapCon = mapHyp.mappingConnection               # get corresponding mapping
-                        driver = self.IDs[mapCon.driverToken]           # Driver token
-                        recipient =self.IDs[mapCon.recipientToken]      # recipient token
-                        mp.append(mapCon.weight)                        # weight
-                        mp.append(mapHyp.hypothesis)                    # hypothesis
-                        mp.append(mapHyp.max_hyp)                       # max_hyp
-                        self.mappings[(driver, recipient)] = mp         # add mapping to list
-                        addedMaps[mapCon] = True                        # mark mapping as added
-                    for mapCon in token.mappingConnections:        # Add any remaining mappings without hypotheses
-                        if addedMaps[mapCon] != True:              # NOTE: need to check when mapping has/hasnt got hypothesis
-                            mp = []                                     # emtpy mapping
-                            driver = self.IDs[mapCon.driverToken]       # Driver token
-                            recipient =self.IDs[mapCon.recipientToken]  # recipient token
-                            mp.append(mapCon.weight)                    # weight
-                            mp.append(0.0)                              # hypothesis
-                            mp.append(0.0)                              # max_hyp
-                            self.mappings[(driver, recipient)] = mp     # add mapping to list
-                            addedMaps[mapCon] = True                    # mark mapping as added
-
-
-                    # Connections
-                    # TODO: add connections
-                    # Match token type, as each token stores info differently.
-                    # Only add child nodes to list so dont duplicate writes
+                for token in tokens:                                 
+                    tk = self.formatToken(token, anumber)               # Format Token
+                    self.nodes.append(tk)   
+                    self.addMappings(token)                             # Add Mappings
+                    self.addConnections(token)                          # Connections
 
     # Returns formatted list of token values
     def formatToken(self, token: dt.TokenUnit, anumber):
         tk = []                                             # Empty token            
         # =====        Set shared properties        =====
-        self.names.append([self.IDs[token], token.name])    # Save name to dictionary
+        self.names[self.IDs.get(token)] = token.name        # Map ID -> name
         # -------------------------------------------- INTs
-        tk.append(self.IDs[token])                          # ID
-        tk.append(self.sets.index[token.set])               # Set
+        tk.append(self.IDs.get(token))                      # ID
+        tk.append("TypeNotSet")                             # Type
+        tk.append(self.sets.index(token.set))               # Set
         tk.append(anumber)                                  # analog
-        tk.append(self.IDs[token.max_map_unit])             # max_map_unit
-        tk.append(self.IDs[token.my_made_unit])             # made_unit
-        tk.append(self.IDs[token.my_maker_unit])            # maker_unit
-        tk.append(self.IDs[token.inhibitor_threshold])      # inhibitor_threshold
+        tk.append(self.IDs.get(token.max_map_unit))         # max_map_unit
+        tk.append(self.IDs.get(token.my_made_unit))         # made_unit
+        tk.append(self.IDs.get(token.my_maker_unit))        # maker_unit
+        tk.append(self.IDs.get(token.inhibitorThreshold))   # inhibitor_threshold
         # ------------------------------------------- BOOLs
-        tk.append(token.inferred)                           #inferred
-        tk.append(token.retrieved)                          #retrieved
-        tk.append(token.copy_for_DR)                        #copy_for_dr
-        tk.append(token.copied_DR_index)                    #copied_dr_index
-        tk.append(token.sim_made)                           #sim_made
-        tk.append(False)                                    #isDeleted = False
+        tk.append(token.inferred)                           # inferred
+        tk.append(token.retrieved)                          # retrieved
+        tk.append(token.copy_for_DR)                        # copy_for_dr
+        tk.append(token.copied_DR_index)                    # copied_dr_index
+        tk.append(token.sim_made)                           # sim_made
+        tk.append(False)                                    # isDeleted = False
         # ------------------------------------------  FLOATs
-        tk.append(token.act)                                #act
-        tk.append(token.max_act)                            #max_act
-        tk.append(token.inhibitor_input)                    #inhibitor_input
-        tk.append(token.inhibitor_act)                      #inhibitor_act
-        tk.append(token.max_map)                            #max_map
-        tk.append(token.td_input)                           #td_input
-        tk.append(token.bu_input)                           #bu_input
-        tk.append(token.lateral_input)                      #lateral_input
-        tk.append(token.map_input)                          #map_input
-        tk.append(token.net_input)                          #net_input
+        tk.append(token.act)                                # act
+        tk.append(token.max_act)                            # max_act
+        tk.append(token.inhibitor_input)                    # inhibitor_input
+        tk.append(token.inhibitor_act)                      # inhibitor_act
+        tk.append(token.max_map)                            # max_map
+        tk.append(token.td_input)                           # td_input
+        tk.append(token.bu_input)                           # bu_input
+        tk.append(token.lateral_input)                      # lateral_input
+        tk.append(token.map_input)                          # map_input
+        tk.append(token.net_input)                          # net_input
         # ====         Set per type properties       =====
         match type(token):
             case dt.Groups:                                 # Groups
+                tk[1] = 4                                   # token.set
                 tk.append(token.group_layer)                # - group layer - INT
             case dt.PUnit:                                  # P
+                tk[1] = 3                                   # token.set
                 tk.append(token.mode)                       # - p.mode - INT
             case dt.RBUnit:                                 # RB
-                tk.append(token.timesfired)                 # - rb.timesfired - INT
+                tk[1] = 2                                   # token.set
+                tk.append(token.timesFired)                 # - rb.timesfired - INT
             case dt.POUnit:                                 # PO
+                tk[1] = 1                                   # token.set
                 tk.append(token.semNormalization)           # - po.semNormalizatino - INT
                 tk.append(bool(token.predOrObj))            # - po.predOrObj -> BOOL
                 tk.append(token.max_sem_weight)             # - max_sem_weight - FLOAT
@@ -157,26 +134,113 @@ class tensorBuilder(object):
         #sem_input
         #max_sem_input
         return None
+    
+    # Adds mappings and hypotheses for a given token
+    def addMappings(self, token):
+        addedMaps = {}
+        for mapHyp in token.mappingHypotheses:              # Add hypotheses
+            mp = []                                         # emtpy mapping
+            mapCon = mapHyp.mappingConnection               # get corresponding mapping
+            driver = self.IDs[mapCon.driverToken]           # Driver token
+            recipient =self.IDs[mapCon.recipientToken]      # recipient token
+            mp.append(mapCon.weight)                        # weight
+            mp.append(mapHyp.hypothesis)                    # hypothesis
+            mp.append(mapHyp.max_hyp)                       # max_hyp
+            self.mappings[(driver, recipient)] = mp         # add mapping to list
+            addedMaps[mapCon] = True                        # mark mapping as added
+        for mapCon in token.mappingConnections:        # Add any remaining mappings without hypotheses
+            if addedMaps[mapCon] != True:              # NOTE: need to check when mapping has/hasnt got hypothesis
+                mp = []                                     # emtpy mapping
+                driver = self.IDs[mapCon.driverToken]       # Driver token
+                recipient =self.IDs[mapCon.recipientToken]  # recipient token
+                mp.append(mapCon.weight)                    # weight
+                mp.append(0.0)                              # hypothesis
+                mp.append(0.0)                              # max_hyp
+                self.mappings[(driver, recipient)] = mp     # add mapping to list
+                addedMaps[mapCon] = True                    # mark mapping as added
 
-    # create the tensor ID for a node
-    def getID(self, node):
-        if node == None:
-            return None
-        node.my_set
-        node.get_index(self.mem)                           # Index token
-        typeIndex = self.tokenTypes.index(type(node))      # How many types assigned ID before this node
-        offset = sum(self.tokenAmounts[:typeIndex])        # Number of nodes assigned ID befor this node
-        return (node.my_index+offset)                      # unique ID, ordered by node type
+    # Adds all connections for token (Only add child nodes so dont duplicate writes)
+    def addConnections(self, token):
+        # TODO add all forward connections for given token
+        # Match token type, as each token stores info differently.
+        match type(token):
+            case dt.POUnit:
+                pass
+            case dt.RBUnit:
+                pass
+            case dt.PUnit:
+                pass
+            case dt.Groups:
+                pass
+            case dt.Semantic:
+                pass
     
     # creates ID for each node and store in hash map for efficent lookup
     def identifyNodes(self):
+        ID = 0
         for type in [self.mem.semantics, self.mem.POs, self.mem.RBs, self.mem.Ps, self.mem.Groups]:
             for node in type:
-                self.IDs[node] = {self.getID(node)}
+                self.IDs[node] = ID
+                print(node, ID)
+                ID += 1
 
     # Returns 0 if not integer, or val o.w
     def sanitiseInt(self, val):
         if type(val) != int:
             return 0
         return val
-                
+    
+    # Print formated token in readable way for debugging
+    def printToken(self, tk):
+        # print all nodes
+        if tk == "-a":
+            for node in self.nodes:
+                self.printToken(node)
+            return
+        # sort nodes by id, then print them all
+        if tk == "-s":
+            for node in sorted(self.nodes, key=lambda x: x[1]):
+                self.printToken(node)
+            return
+
+        properties = [
+            "ID",
+            "Type",
+            "Set",
+            "analog",
+            "max_map_unit",
+            "made_unit",
+            "maker_unit",
+            "inhibitor_threshold",
+            "inferred",
+            "retrieved",
+            "copy_for_dr",
+            "copied_dr_index",
+            "sim_made",
+            "isDeleted",
+            "act",
+            "max_act",
+            "inhibitor_input",
+            "inhibitor_act",
+            "max_map",
+            "td_input",
+            "bu_input",
+            "lateral_input",
+            "map_input",
+            "net_input"
+        ]
+
+        # Find the max length of property names for alignment
+        max_key_length = max(len(prop) for prop in properties)
+
+        # Create header box for ID
+        id_value = tk[0]  # Assuming the first element corresponds to "ID"
+        id_box = f"---=--------- ID : {id_value} ----------"
+        print(id_box)
+
+        # Print each property aligned
+        for i in range(len(properties)):
+            print(f"{str(i).ljust(3)}| {properties[i].ljust(max_key_length)} = {tk[i]}")
+
+        print(("-" * 3) + "=" + ("-" * (len(id_box) - 4)))  # Bottom order
+
