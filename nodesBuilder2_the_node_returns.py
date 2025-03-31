44d45ceb39e9bf65bdee44173d85df09daca773f
miles rose <milespublic@pm.me>
2025-03-31 19:11:04 +0100

    bug fixes

--- nodesBuilder2_the_node_returns.py
+++ nodesBuilder2_the_node_returns.py
@@ -6,51 +6,55 @@ from nodeTensors import *
 from nodeMemObjects import Links
 
 # ===========[ INTERMIDIATE DATA STRUCTURES ]===========
-
 class Node(object):
     def __init__(self, name):
         self.name = name
         self.features = None
+        self.ID = None
 
     def floatate_features(self):
         for feature in self.features:
             if feature is not None:
                 feature = float(feature)
             else:
+                print("None type in node:", self.name)
                 feature = 0.0
     
-    def set(self, feature: TF, value: float):
+    def set(self, feature, value: float):
         self.features[feature] = value
-
+    
+    def set_ID(self, ID):
+        self.ID = ID
+        self.features[TF.ID] = ID
 
 class Semantic(Node):
-    def __init__(self, name, ID):
+    def __init__(self, name):
         super().__init__(name)
         self.features = [None] * len(SF)
-        self.initialise_defaults(ID)
+        self.initialise_defaults()
     
-    def initialise_defaults(self, ID):
-        self.features[SF.ID] = ID
+    def initialise_defaults(self):      # TODO: Check defaults
         self.features[SF.TYPE] = Type.SEMANTIC
-        self.features[SF.ONT_STATUS] = None
+        self.features[SF.ONT_STATUS] = 0
         self.features[SF.AMOUNT] = 0
         self.features[SF.INPUT] = 0
         self.features[SF.MAX_INPUT] = 0
         self.features[SF.ACT] = 0
 
-
 class Token(Node):
-    def __init__(self, name, ID):
+    def __init__(self, name, set: Set, analog: int):
         super().__init__(name)
         self.features = [None] * len(TF)
-        self.initialise_defaults(ID)
+        self.initialise_defaults()
+        self.set(TF.SET, set)
+        self.set(TF.ANALOG, analog)
         self.children = []
     
-    def initialise_defaults(self, ID):
-        self.features[TF.ID] = ID
-        self.features[TF.TYPE] = Type.TOKEN
+    def initialise_defaults(self):   # TODO: Check defaults
+        self.features[TF.ID] = None
+        self.features[TF.TYPE] = None
         self.features[TF.SET] = None
-        self.features[TF.ANALOG] = 0
+        self.features[TF.ANALOG] = None
         self.features[TF.MAX_MAP_UNIT] = 0
         self.features[TF.MADE_UNIT] = 0
         self.features[TF.MAKER_UNIT] = 0
@@ -73,25 +77,22 @@ class Token(Node):
         self.features[TF.SIM_MADE] = False
         self.features[TF.DELETED] = False
         self.features[TF.PRED] = False
-
-
+    
 class Prop(Token):
-    def __init__(self, name, ID):
-        super().__init__(name, ID)
-        self.features[TF.TYPE] = Type.P
-
+    def __init__(self, name, set, analog):
+        super().__init__(name, set, analog)
+        self.set(TF.TYPE, Type.P)
 
 class RB(Token):
-    def __init__(self, name, ID):
-        super().__init__(name, ID)
-        self.features[TF.TYPE] = Type.RB
-
+    def __init__(self, name, set, analog):
+        super().__init__(name, set, analog)
+        self.set(TF.TYPE, Type.RB)
 
 class PO(Token):
-    def __init__(self, name, ID, is_pred: bool):
-        super().__init__(name, ID)
-        self.features[TF.TYPE] = Type.PO
-        self.features[TF.PRED] = B.TRUE if is_pred else B.FALSE
+    def __init__(self, name, set, analog, is_pred: bool):
+        super().__init__(name, set, analog)
+        self.set(TF.TYPE, Type.PO)
+        self.set(TF.PRED, is_pred)
 
 class Token_set(object):
     def __init__(self, set: Set, tokens: dict[Type, list[Token]], name_dict: dict[str, Token], id_dict: dict[int, Token]):
@@ -113,6 +114,7 @@ class Token_set(object):
         token_tensor = torch.zeros((self.num_tokens, len(TF)))
         for type in Type:
             for token in self.tokens[type]:
+                token.floatate_features()
                 token_tensor[token.ID] = torch.tensor(token.features)
         return token_tensor
     
@@ -126,13 +128,21 @@ class Sem_set(object):
         self.sems = sems
         self.name_dict = name_dict
         self.id_dict = id_dict
-
+        self.num_sems = len(sems)
+        self.connections = np.zeros((self.num_sems, self.num_sems))
+    
     def get_sem(self, name):
         return self.name_dict[name]
     
     def get_sem_by_id(self, ID):
         return self.id_dict[ID]
 
+    def tensorise(self):
+        self.node_tensor = torch.zeros((self.num_sems, len(SF)))
+        self.connections_tensor = torch.tensor(self.connections)
+        for sem in self.sems:
+            sem.floatate_features()
+            self.node_tensor[sem.ID] = torch.tensor(sem.features)
 # ------------------------------------------------------
 
 
@@ -140,80 +150,79 @@ class Sem_set(object):
 class Build_set(object):                                # Builds the nodes for a given set
     def __init__(self, symProps: list[dict], set: Set):
         self.symProps = symProps
-        self.props = []
-        self.RBs = []
-        self.preds = []
-        self.objs = []
         self.tokens = {}
         self.set = set
         self.name_dict = {}
         self.id_dict = {}
-        self.type_map = {
-            Type.P: Prop,
-            Type.RB: RB,
-            Type.PO: PO
-        }
+        self.names = []
+        self.token_set = None
     
     def build_set(self):    #  Returns a new token_set
-        self.get_nodes(self.symProps)                                           # Get the nodes from the symProps
-        self.create_tokens()                                                     # Create basic tokens
-        return Token_set(self.set, self.props, self.RBs, self.preds, self.objs, self.name_dict, self.id_dict) #  Return new token_set
+        self.get_nodes()       # Get the nodes from the symProps
+        self.id_tokens()                    # Create basic tokens
+        self.token_set = Token_set(self.set, self.tokens, self.name_dict, self.id_dict)   # Return new token_set
+        return self.token_set
 
-    
-    def get_nodes(self, symProps: list[dict]):              # Gets lists of unique tokens by type
-        for type in Type:                                   # Initialise empty lists for each type
+
+    def get_nodes(self):         # Gets lists of unique tokens by type
+        for type in Type:                    # Initialise empty lists for each type
             self.tokens[type] = []
+        
         non_exist = "non_exist"
         for prop in self.symProps:
             prop_name = prop['name']
+            analog = prop['analog']
             if prop_name != non_exist:
-                self.tokens[Type.P].append(prop_name)
+                self.create_token(prop_name, Prop, analog)
             for rb in prop['RBs']:
                 pred_name = rb['pred_name']
-                if pred_name != non_exist:
-                    self.tokens[Type.PO].append((pred_name, True))
                 obj_name = rb['object_name']
+                self.create_token(pred_name + "_" + obj_name, RB, analog)
+                if pred_name != non_exist:
+                    self.create_token(pred_name, PO, analog, True)
                 if obj_name != non_exist:
-                    self.tokens[Type.PO].append((obj_name, False))
-                self.tokens[Type.RB].append(pred_name + "_" + obj_name)
-        for type in Type:
-            self.tokens[type] = list(set(self.tokens[type])) # Remove duplicates
+                    self.create_token(obj_name, PO, analog, False)
 
-    def create_tokens(self):                                 # Create basic tokens, based on default values
+
+    def id_tokens(self):   # Give each token an ID
+        self.names = list(set(self.names))
         i = 0
-        for type in Type:
-            for token in self.tokens[type]:
-                self.create_token(token, i, type)
-                i += 1
+        for name in self.names:
+            node_obj =  self.name_dict[name]
+            node_obj.set(TF.ID, i)
+            i += 1
 
-    def create_token(self, name, ID, type):             # Create a token and add it to the name and id dictionaries
-        if isinstance(name, tuple):                     # If name is a tuple, it is a PO object, as contains name and is_pred
-            obj = PO(name[0], ID, name[1])
-        else:                                           # Otherwise, it is a basic token
-            obj_class = self.type_map[type]
-            obj = obj_class(name, ID)
+    def create_token(self, name, token_class, analog, is_pred = None): # Create a token and add it to the name/dict
+        if is_pred is not None:
+            obj = token_class(name, self.set, analog, is_pred)
+        else:
+            obj = token_class(name, self.set, analog)
         self.name_dict[name] = obj
-        self.id_dict[ID] = obj
+        self.names.append(name)
 
 
 class Build_sems(object):                               # Builds the semantic objects
     def __init__(self, symProps: list[dict]):
         self.sems = []
         self.nodes = []
-        self.node_dict = {}
+        self.name_dict = {}
         self.id_dict = {}
         self.num_sems = 0
+        self.symProps = symProps
     
     def build_sems(self):
         self.get_sems(self.symProps)
         self.tokenise()
-        return Sem_set(self.nodes, self.name_dict, self.id_dict)
+        self.sem_set = Sem_set(self.nodes, self.name_dict, self.id_dict)
+        return self.sem_set
 
     def tokenise(self):                                 # Turn each unique semantic into a semantic object:
+        self.num_sems = 0
         for sem in self.sems:
-            self.id_dict[sem] = self.num_sems
-            new_sem = Semantic(sem, self.num_sems)
+            new_sem = Semantic(sem)
+            new_sem.set_ID(self.num_sems)
             self.nodes.append(new_sem)
+            self.id_dict[self.num_sems] = new_sem
             self.name_dict[sem] = new_sem
             self.num_sems += 1
     
@@ -229,10 +238,6 @@ class Build_sems(object):                               # Builds the semantic ob
 
         self.sems = list(set(self.sems))
         self.num_sems = len(self.sems)
-    
-    def build(self):
-        self.get_sems(self.symProps)
-        self.tokenise()
 
 
 class Build_children(object):                           # Builds the children of each token
@@ -257,15 +262,17 @@ class Build_children(object):                           # Builds the children of
                 prop_obj.children.append(child)             # Append child ID to prop object
 
     def get_rb_children(self, rb: dict):                    # Add children to the rb object, recursively call lower get child functions
-        rb_obj = self.get_object(rb['object_name'] + "_" + rb['pred_name'])
-        child_pred = self.get_po_children(rb['pred_name'], rb['pred_sem'])      # Get children of pred, return pred ID
-        child_obj = self.get_po_children(rb['object_name'], rb['object_sem'])   # Get children of obj, return obj ID
+        pred_name = rb['pred_name']
+        obj_name = rb['object_name']
+        rb_obj = self.get_object(pred_name + "_" + obj_name)
+        child_pred = self.get_po_children(pred_name, rb['pred_sem'])      # Get children of pred, return pred ID
+        child_obj = self.get_po_children(obj_name, rb['object_sem'])   # Get children of obj, return obj ID
         rb_obj.children.append(child_pred)
         rb_obj.children.append(child_obj)
         return rb_obj.ID
 
-    def get_po_children(self, obj: dict, sems: list):       # Add children to the po object, return po ID
-        po_obj = obj['name']
+    def get_po_children(self, name, sems: list):       # Add children to the po object, return po ID
+        po_obj = self.get_object(name)
         for sem in sems:
             sem_obj = self.sems.get_sem(sem)
             po_obj.children.append(sem_obj.ID)
@@ -276,7 +283,7 @@ class Build_children(object):                           # Builds the children of
         non_exist_rb = "non_exist_non_exist"
         obj = None
         if name != non_exist and name != non_exist_rb:
-            obj = self.token_set.get_node(name)
+            obj = self.token_set.get_token(name)
         return obj  
 
 
@@ -313,6 +320,7 @@ class Build_connections(object):                        # Builds links and conne
 class Build_tensors(object):
     def __init__(self, symProps: list[dict]):
         self.symProps = symProps
+        self.token_sets = {}
         self.set_map = {
             "driver": Set.DRIVER,
             "recipient": Set.RECIPIENT,
@@ -324,7 +332,7 @@ class Build_tensors(object):
         self.build_set_tensors()
         self.build_mem_objects()
         self.build_node_tensors()
-        self.nodes = Nodes(self.driver_tensor, self.recipient_tensor, self.memory_tensor, self.new_set_tensor, self.semantics, self.links, self.mappings)
+        self.nodes = Nodes(self.driver_tensor, self.recipient_tensor, self.memory_tensor, self.new_set_tensor, self.semantics_tensor, self.mappings)
         return self.nodes
 
     def build_set_tensors(self):    # Build sem_set, token_sets
@@ -332,7 +340,7 @@ class Build_tensors(object):
 
         # 1). Build the sems
         build_sems = Build_sems(self.symProps)
-        sems = build_sems.build()
+        self.sems = build_sems.build_sems()
 
         # 2). Initiliase empty lists for each set
         for set in Set:                                 
@@ -344,22 +352,22 @@ class Build_tensors(object):
         
         # 4). Build the basic token sets
         for set in Set:                                
-            build_set = Build_set(self.props[set], set)
+            build_set = Build_set(props[set], set)
             self.token_sets[set] = build_set.build_set()
         
         # 5). Build the children lists of IDs
         for set in Set:
-            build_children = Build_children(set, self.token_sets[set], sems, self.props[set])
+            build_children = Build_children(set, self.token_sets[set], self.sems, props[set])
             build_children.get_children()
         
-        # 6). Build the connections and links matrices
-        for set in Set:                                 
-            build_connections = Build_connections(self.token_sets[set], self.sems)
-            build_connections.build_connections_links()
+        # 6). Build the connections and links matrices                              
+        build_connections = Build_connections(self.token_sets, self.sems)
+        build_connections.build_connections_links()
         
         # 7). Tensorise the sets
         for set in Set:
             self.token_sets[set].tensorise()
+        self.sems.tensorise()
 
     def build_mem_objects(self):    # Build the mem objects
         self.links = Links(self.token_sets[Set.DRIVER].links, self.token_sets[Set.RECIPIENT].links, self.token_sets[Set.MEMORY].links)
@@ -367,18 +375,18 @@ class Build_tensors(object):
 
     def build_node_tensors(self):   # Build the node tensor objects
         driver_set = self.token_sets[Set.DRIVER]
-        self.driver_tensor = DriverTensor(driver_set.token_tensor, driver_set.connections_tensor)
+        self.driver_tensor = DriverTensor(driver_set.token_tensor, driver_set.connections_tensor, self.links)
         
         recipient_set = self.token_sets[Set.RECIPIENT]
-        self.recipient_tensor = RecipientTensor(recipient_set.token_tensor, recipient_set.connections_tensor)
+        self.recipient_tensor = RecipientTensor(recipient_set.token_tensor, recipient_set.connections_tensor, self.links)
 
         memory_set = self.token_sets[Set.MEMORY]
-        self.memory_tensor = TokenTensor(memory_set.token_tensor, memory_set.connections_tensor)
+        self.memory_tensor = TokenTensor(memory_set.token_tensor, memory_set.connections_tensor, self.links)
 
         new_set = self.token_sets[Set.NEW_SET]
-        self.new_set_tensor = TokenTensor(new_set.token_tensor, new_set.connections_tensor)
+        self.new_set_tensor = TokenTensor(new_set.token_tensor, new_set.connections_tensor, self.links)
 
-        self.semantics = SemanticTensor(self.sems.nodes, self.sems.connections, self.links)
+        self.semantics_tensor = SemanticTensor(self.sems.node_tensor, self.sems.connections_tensor, self.links)
 # ------------------------------------------------------
 
 # ===================[ MAIN FUNCTION ]==================
@@ -388,6 +396,7 @@ def main(symProps: list[dict]):
     return nodes
 
 if __name__ == "__main__":
+    # example symProps
     symProps = [{'name': 'lovesMaryTom', 'RBs': [{'pred_name': 'lover', 'pred_sem': ['lover1', 'lover2', 'lover3'], 'higher_order': False, 'object_name': 'Mary', 'object_sem': ['mary1', 'mary2', 'mary3'], 'P': 'non_exist'}, {'pred_name': 'beloved', 'pred_sem': ['beloved1', 'beloved2', 'beloved3'], 'higher_order': False, 'object_name': 'Tom', 'object_sem': ['tom1', 'tom2', 'tome3'], 'P': 'non_exist'}], 'set': 'driver', 'analog': 0}, 
     {'name': 'lovesTomJane', 'RBs': [{'pred_name': 'lover', 'pred_sem': ['lover1', 'lover2', 'lover3'], 'higher_order': False, 'object_name': 'Tom', 'object_sem': ['tom1', 'tom2', 'tome3'], 'P': 'non_exist'}, {'pred_name': 'beloved', 'pred_sem': ['beloved1', 'beloved2', 'beloved3'], 'higher_order': False, 'object_name': 'Jane', 'object_sem': ['jane1', 'jane2', 'mary2'], 'P': 'non_exist'}], 'set': 'driver', 'analog': 0},
     {'name': 'jealousMaryJane', 'RBs': [{'pred_name': 'jealous_act', 'pred_sem': ['jel1', 'jel2', 'jel3'], 'higher_order': False, 'object_name': 'Mary', 'object_sem': ['mary1', 'mary2', 'mary3'], 'P': 'non_exist'}, {'pred_name': 'jealous_pat', 'pred_sem': ['jel4', 'jel5', 'jel6'], 'higher_order': False, 'object_name': 'Jane', 'object_sem': ['jane1', 'jane2', 'mary2'], 'P': 'non_exist'}], 'set': 'driver', 'analog': 0},
@@ -395,3 +404,4 @@ if __name__ == "__main__":
     {'name': 'lovesKathySam', 'RBs': [{'pred_name': 'lover', 'pred_sem': ['lover1', 'lover2', 'lover3'], 'higher_order': False, 'object_name': 'Kathy', 'object_sem': ['kathy1', 'kathy2', 'kathy3'], 'P': 'non_exist'}, {'pred_name': 'beloved', 'pred_sem': ['beloved1', 'beloved2', 'beloved3'], 'higher_order': False, 'object_name': 'Sam', 'object_sem': ['sam1', 'sam2', 'sam3'], 'P': 'non_exist'}], 'set': 'recipient', 'analog': 0}]
 
     main(symProps)
+# ------------------------------------------------------
