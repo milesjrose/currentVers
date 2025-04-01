0570dacdaf40cf67629318a5ee2e213306be5388
miles rose <milespublic@pm.me>
2025-04-01 16:59:48 +0100

    flesh out printer to print connections, and links

--- nodePrinter.py
+++ nodePrinter.py
@@ -1,12 +1,50 @@
 from nodes import *
-
+from nodeEnums import *
+import os
 
 class nodePrinter(object):
-    def __init__(self, nodes: Nodes):
+    def __init__(self, nodes: Nodes, print_to_console: bool = True, log_file: str = None):
         self.nodes = nodes
-    
-    def print_tk_tensor(self, tensor: torch.Tensor, types = None):
-        headers = f"{tensor.shape[0]}x{tensor.shape[1]} Tensor:"
+        self.print_to_console = print_to_console
+        self.log_file = log_file
+
+    def print_token_tensor(self, set: Set, types = None, mask = None, label_values = True, label_names = True, headers = None, print_cons = True, cons_headers = None, links_headers = None):
+        tensor = self.nodes.sets[set]
+        if headers is None:
+            headers = [f"{set.name} Tokens:"]
+            if cons_headers is None:
+                cons_headers = [f"{set.name} Connections:"]
+            if links_headers is None:
+                links_headers = [f"{set.name} Links:"]
+        
+        if types is None:
+            types = [TF.ID, TF.TYPE, TF.SET, TF.ANALOG, TF.PRED]
+        if mask is not None:
+            tokens = tensor.nodes[mask]
+            if print_cons:
+                cons = tensor.connections[mask, :]
+                links = tensor.links.sets[set]
+        else:
+            tokens = tensor.nodes
+            if print_cons:
+                cons = tensor.connections
+                links = tensor.links.sets[set]
+        if label_names:
+            names = tensor.names
+        else:
+            names = None
+        
+        self.print_tk_tensor(tokens, types=types, label_values=label_values, names=names, headers=headers)
+        if print_cons:
+            self.print_con_tensor(cons, mask=mask, names=names, headers=cons_headers)
+            self.print_links_tensor(links, mask=mask, names=names, headers=links_headers)
+        
+        
+
+    def print_tk_tensor(self, tensor: torch.Tensor, types = None, label_values = True, names = None, headers = None):
+        if headers is None:
+            headers = [f"Tokens Tensor:"]
+        
         if types is None:
             columns = [TF.name for TF in TF]
         else:
@@ -16,10 +54,78 @@ class nodePrinter(object):
         if len(rows) == 0:
             print("No tokens found")
             return
-        headers = ["Tokens:"]
-        printer = tablePrinter(columns, rows, headers)
+        if label_values:
+            rows, success = self.label_values(rows, types, names)
+            if names is not None:
+                if success:
+                    columns.insert(1, "Name")
+        printer = tablePrinter(columns, rows, headers, self.log_file, self.print_to_console)
         printer.print_table()
     
+    def print_con_tensor(self, tensor: torch.Tensor, mask = None, names = None, headers = None):
+        if names is not None:
+            pass
+        if headers is None:
+            headers = ["Connections:"]
+        if mask is not None:
+            pass
+
+        rows = tensor.tolist()
+        for r, row in enumerate(rows):
+            for i in range(len(row)):
+                if row[i] == 1:
+                    row[i] = "x"
+                else:
+                    row[i] = " "
+            if names is not None:
+                row.insert(0, names[r])
+            else:
+                row.insert(0, r)
+        
+        columns = ["P-> C:"]
+        for i in range(len(rows[0])-1):
+            if names is not None:
+                columns.append(names[i])
+            else:
+                columns.append(i)
+        
+        printer = tablePrinter(columns, rows, headers, self.log_file, self.print_to_console)
+        printer.print_table(split=True)
+    
+    def print_links_tensor(self, tensor: torch.Tensor, mask = None, names = None, headers = None):
+        sem_names = None
+        if names is not None:
+            sem_names = self.nodes.semantics.names
+        if headers is None:
+            headers = ["Links:"]
+        
+        if mask is not None:
+            pass
+
+        rows = tensor.tolist()
+        for r, row in enumerate(rows):
+            for i in range(len(row)):
+                if row[i] == 1:
+                    row[i] = "x"
+                else:
+                    row[i] = " "
+            if names is not None:
+                row.insert(0, names[r])
+            else:
+                row.insert(0, r)
+        
+        columns = ["P-> C:"]
+        for i in range(len(rows[0])-1):
+            if names is not None:
+                columns.append(sem_names[i])
+            else:
+                columns.append(i)
+
+        printer = tablePrinter(columns, rows, headers, self.log_file, self.print_to_console)
+        printer.print_table(split=True)
+        
+            
+
     def print_tokens(self, set: Set, token_ids: list[int], types: list[TF]):
         columns = [str(type.name) for type in types]
         tokens = self.nodes.sets[set].nodes
@@ -29,9 +135,46 @@ class nodePrinter(object):
         if len(rows) == 0:
             print("No tokens found")
             return
+        
         headers = [f"Set:{set.name}"] + ["Tokens:"] +[f"{token_ids}"]
-        printer = tablePrinter(columns, rows, headers)
+        printer = tablePrinter(columns, rows, headers, self.log_file, self.print_to_console)
         printer.print_table()
+    
+    def label_values(self, row:list[float], types:list[TF], names:dict[int, str]):
+        labels =  {
+            # Labeled values:
+            TF.TYPE: Type,
+            TF.SET: Set,
+            TF.MODE: Mode,
+            #  Bool values:
+            TF.INFERRED: B,
+            TF.RETRIEVED: B,
+            TF.COPY_FOR_DR: B,
+            TF.COPIED_DR_INDEX: B,
+            TF.SIM_MADE: B,
+            TF.DELETED: B,
+            TF.PRED: B,
+        }
+        
+        # Label values
+        for t, type in enumerate(types):
+            for i, value in enumerate(row):
+                try:
+                    row[i][t] = labels[type](value[t]).name
+                except KeyError:
+                    pass
+                success = True
+    
+        # Add name column to row
+        if names is not None:
+            try:
+                if types[0] == TF.ID:
+                    for i in range(len(row)):
+                        row[i].insert(1, names[row[i][0]])
+            except:
+                success = False
+
+        return row, success
 
 # ====================[ PRINTER ENUMS ]=====================
 class lineTypes(IntEnum):
@@ -54,15 +197,16 @@ class C(IntEnum):
     VERTICAL_RIGHT = 10
 # =================[ TABLE PRINTER CLASS ]==================
 class tablePrinter(object):
-    def __init__(self, columns: list[str], rows: list[list[str]], headers: list[str]):
-        self.columns = columns
-        self.rows = rows
-        self.headers = headers
+    def __init__(self, columns: list[str], rows: list[list[str]], headers: list[str], log_file: str = None, print_to_console: bool = True):
         self.col_widths = None
         self.header_widths = None
         self.dp = 2
         self.padding = 4
-
+        self.columns = self.format(columns)
+        self.rows = self.format_rows(rows)
+        self.headers = self.format(headers)
+        self.log_file = log_file
+        self.print_to_console = print_to_console
         self.chars = {
             "header" : ["╒","╕","╘","╛","═","│","╪", "╤", "╧", "╣", "╠"],
             "table"  : ["┌","┐","└","┘","─","│","┼", "┬", "┴", "┤", "├"]
@@ -75,44 +219,58 @@ class tablePrinter(object):
             lineTypes.BOTTOM: [C.BOTTOM_LEFT, C.BOTTOM_RIGHT, C.HORIZONTAL, C.HORIZONTAL_UP],
             lineTypes.SPLIT: [C.VERTICAL_RIGHT, C.VERTICAL_LEFT, C.HORIZONTAL, C.CROSS],
         }
+    
+    def format(self, data: list[str]):
+        for i, value in enumerate(data):
+            if isinstance(value, float):
+                data[i] = f"{value:.{self.dp}f}"
+            else:
+                data[i] = str(value)
+        return data
+
+    
+    def format_rows(self, rows: list[list[str]]):
+        for i, row in enumerate(rows):
+            rows[i] = self.format(row)
+        return rows
 
     def print_header(self, char_set= "header"):
         if self.header_widths is None:
             self.calc_header_width()
-        print(self.get_line(lineTypes.TOP, char_set, self.header_widths))
-        print(self.get_line(lineTypes.MIDDLE, char_set, self.header_widths, format_data=self.headers))
-        print(self.get_line(lineTypes.BOTTOM, char_set, self.header_widths))
+        self.get_line(lineTypes.TOP, char_set, self.header_widths)
+        self.get_line(lineTypes.MIDDLE, char_set, self.header_widths, format_data=self.headers)
+        self.get_line(lineTypes.BOTTOM, char_set, self.header_widths)
     
     def print_column_names(self, split= True, char_set="table"):
         if self.col_widths is None:
             self.calc_col_widths()
-        print(self.get_line(lineTypes.TOP, char_set, self.col_widths))
-        print(self.get_line(lineTypes.MIDDLE, char_set, self.col_widths, format_data=self.columns))
+        self.get_line(lineTypes.TOP, char_set, self.col_widths)
+        self.get_line(lineTypes.MIDDLE, char_set, self.col_widths, format_data=self.columns)
         if split:
-            print(self.get_line(lineTypes.SPLIT, char_set, self.col_widths))
+            self.get_line(lineTypes.SPLIT, char_set, self.col_widths)
         else:
-            print(self.get_line(lineTypes.BOTTOM, char_set, self.col_widths))
+            self.get_line(lineTypes.BOTTOM, char_set, self.col_widths)
     
-    def print_rows(self, print_top=False, print_bottom=True, char_set="table"):
+    def print_rows(self, print_top=False, print_bottom=True, char_set="table", split=False):
         if self.col_widths is None:
             self.calc_col_widths()
         if print_top:
-            print(self.get_line(lineTypes.TOP   , char_set, self.col_widths))
-        for row in self.rows:
-            print(self.get_line(lineTypes.MIDDLE, char_set, self.col_widths, format_data=row))
+            self.get_line(lineTypes.TOP   , char_set, self.col_widths)
+        for i, row in enumerate(self.rows):
+            self.get_line(lineTypes.MIDDLE, char_set, self.col_widths, format_data=row)
+            if split and i != len(self.rows) - 1:
+                self.get_line(lineTypes.SPLIT, char_set, self.col_widths)
         if print_bottom:
-            print(self.get_line(lineTypes.BOTTOM, char_set, self.col_widths))
+            self.get_line(lineTypes.BOTTOM, char_set, self.col_widths)
     
-    def print_table(self, header=True, column_names=True, header_char_set="header", column_char_set="table", row_char_set="table"):
+    def print_table(self, header=True, column_names=True, header_char_set="header", column_char_set="table", row_char_set="table", split=False):
         if self.col_widths is None:
             self.calc_col_widths()
         if header:
             self.print_header(char_set=header_char_set)
         if column_names:
             self.print_column_names(char_set=column_char_set)
-            self.print_rows(char_set=row_char_set)
-        else:
-            self.print_rows(char_set=row_char_set)
+        self.print_rows(char_set=row_char_set, split=split, print_top=not(column_names))
 
     def calc_col_widths(self):
         self.col_widths = [0] * len(self.columns)
@@ -134,7 +292,10 @@ class tablePrinter(object):
         col_len = len(self.columns)
         for row in self.rows:
             if len(row) != col_len:
-                raise ValueError("All rows must have the same number of columns")
+                for r in self.rows:
+                    print(r, len(r))
+                print(self.columns, len(self.columns))
+                raise ValueError(f"Row/Column length mismatch row_len:{len(row)} != col_len:{col_len} at row {row, enumerate(row)}")
     
     def calc_header_width(self):
         self.header_widths = [0] * len(self.headers)
@@ -143,9 +304,15 @@ class tablePrinter(object):
 
     def get_line(self, line_type: lineTypes, char_set: str, widths, format_data = None):
         if format_data is None:
-            return self.get_line_no_data(line_type, char_set, widths)
+            line = self.get_line_no_data(line_type, char_set, widths)
         else:
-            return self.get_line_with_data(line_type, char_set, widths, format_data)
+            line = self.get_line_with_data(line_type, char_set, widths, format_data)
+        if self.log_file is not None:
+            with self.open_file(self.log_file) as f:
+                f.write(str(line) + "\n")
+        if self.print_to_console:
+            print(line)
+
     
     def get_line_with_data(self, line_type: lineTypes, char_set: str, widths, format_data):
         leftc, fillc, splitc, rightc = self.get_line_chars(line_type, char_set)
@@ -179,3 +346,10 @@ class tablePrinter(object):
         splitc = chars[char_list[3]]
         return leftc, fillc, splitc, rightc
 
+    def open_file(self, filename):
+        if os.path.exists(filename):
+            f = open(filename, "a", encoding='utf-8')
+        else:
+            f = open(filename, "x", encoding='utf-8')
+        return f
+    
