8ae1a0c8bfe32ef826f37c0cf6c82b6711549b36
miles rose <milespublic@pm.me>
2025-04-03 00:26:12 +0100

    add reference node types

--- nodes/nodeMemObjects.py
+++ nodes/nodeMemObjects.py
@@ -159,3 +159,34 @@ class New_Semantic(object):
         self.tensor[SF.TYPE] = Type.SEMANTIC
         for feature in features:
             self.tensor[feature] = features[feature]
+
+class Ref_Node(object):
+    """
+    A class for referencing a single node, to make it easier to port old code.
+    Only holds set and ID, to find in tensors.
+
+    Attributes:
+        set (Set): The set of the node.
+        ID (int): The ID of the node.
+    """
+    def __init__(self, set: Set, ID: int):
+        """
+        Initialize the Ref_Node object.
+
+        Args:
+            set (Set): The set of the node.
+            ID (int): The ID of the node.
+        """
+        self.set = set
+        self.ID = ID
+
+class Ref_Semantic(object):
+    """
+    A class for referencing a single semantic, to make it easier to port old code.
+    Only holds ID, to find in semantic tensor.
+
+    Attributes:
+        ID (int): The ID of the semantic.
+    """
+    def __init__(self, ID: int):
+        self.ID = ID
