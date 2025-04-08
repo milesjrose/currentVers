2b2f311212e9fab062bd76d1a3f66f34d7bab902
miles rose <milespublic@pm.me>
2025-04-08 17:36:35 +0100

    refactor

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -8,9 +8,9 @@ Provides classes for representing nodes in a network.
 - NetworkBuilder: Builds the network object.
 """
 
-from .network import Network
-from .network_params import Params
-from .builder import NetworkBuilder
+from .network.network import Network
+from .network.network_params import Params
+from .builder.network_builder import NetworkBuilder
 
 __all__ = [
     "Network",
