4742ee5152925c5b9ef30babb268292821207e86
miles rose <milespublic@pm.me>
2025-04-08 21:32:46 +0100

    nodes gives access to enums.

--- DORA_tensorised/nodes/__init__.py
+++ DORA_tensorised/nodes/__init__.py
@@ -3,18 +3,41 @@ Package: nodes
 
 Provides classes for representing nodes in a network.
 
+Classes:
 - Network: Represents the network of nodes.
 - Params: Holds shared parameters for the network.
+
+Functions:
 - build_network: Builds the network object, takes either a file or a list of symProps.
+
+node Enums:
+- semFields: Semantic fields.
+- tokenFields: Token fields.
+
+feature Enums:
+- Type: Types of nodes.
+- Set: Sets in the network.
+- Mode: P token mode.
+- OntStatus: Semantic ont status.
+
+
 """
 
 from .network.network import Network
 from .network.network_params import Params
 from .builder.run_build import build_network
+from .enums import SF as semFields, TF as tokenFields, MappingFields, Type, Set, Mode, OntStatus
 
 __all__ = [
     "Network",
     "Params",
-    "build_network"
+    "build_network",
+    "semFields",
+    "tokenFields",
+    "MappingFields",
+    "Type",
+    "Set",
+    "Mode",
+    "OntStatus"
 ]
 
