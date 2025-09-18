eab086e4ab902e5beff085a3b5c49511772d84a4
milesjrose <s2093547@ed.ac.uk>
2025-09-18 09:32:28 +0100

    update inhibitor ops to use new function calling style

--- DORA_tensorised/nodes/network/operations/inhibitor_ops.py
+++ DORA_tensorised/nodes/network/operations/inhibitor_ops.py
@@ -2,6 +2,10 @@
 # Inhibitor operations for Network class
 
 from ...enums import *
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ..network import Network
 
 class InhibitorOperations:
     """
@@ -16,9 +20,7 @@ class InhibitorOperations:
         Args:
             network: Reference to the Network object
         """
-        self.network = network
-        self.sets = network.sets
-        # NOTE: Not sure where/if these are used? 
+        self.network: 'Network' = network
         self.local = 0.0
         self.glbal = 0.0
 
@@ -30,44 +32,44 @@ class InhibitorOperations:
         - Only updates act of RB in driver.
         """
         #update input driver/recipient RBs, POs
-        self.sets[Set.DRIVER].update_inhibitor_input([Type.RB, Type.PO])
-        self.sets[Set.RECIPIENT].update_inhibitor_input([Type.RB, Type.PO])
+        self.network.driver().update_ops.update_inhibitor_input([Type.RB, Type.PO])
+        self.network.recipient().update_ops.update_inhibitor_input([Type.RB, Type.PO])
         #update driver rb, PO inhibitor act only if in DORA mode
-        if self.DORA_mode:
-            self.sets[Set.DRIVER].update_inhibitor_act([Type.RB, Type.PO])
-            self.sets[Set.RECIPIENT].update_inhibitor_act([Type.PO])
+        if self.network.params.as_DORA:
+            self.network.driver().update_ops.update_inhibitor_act([Type.RB, Type.PO])
+            self.network.recipient().update_ops.update_inhibitor_act([Type.PO])
         else:
-            self.sets[Set.DRIVER].update_inhibitor_act([Type.RB])
+            self.network.driver().update_ops.update_inhibitor_act([Type.RB])
 
     def reset(self):                                                    # Reset inhibitors (for RB and PO units) NOTE: Check if required to set for memory and new_set
         """
         Reset the inhibitors (for RB and PO units).
         (driver, recipient, new_set, memory)
         """
-        self.sets[Set.DRIVER].reset_inhibitor([Type.RB, Type.PO])
-        self.sets[Set.RECIPIENT].reset_inhibitor([Type.RB, Type.PO])
-        self.sets[Set.MEMORY].reset_inhibitor([Type.RB, Type.PO])
-        self.sets[Set.NEW_SET].reset_inhibitor([Type.RB, Type.PO])
+        self.network.driver().update_ops.reset_inhibitor([Type.RB, Type.PO])
+        self.network.recipient().update_ops.reset_inhibitor([Type.RB, Type.PO])
+        self.network.memory().update_ops.reset_inhibitor([Type.RB, Type.PO])
+        self.network.new_set().update_ops.reset_inhibitor([Type.RB, Type.PO])
 
     def check_local(self):                                              # Check local inhibition
         """Check local inhibitor activation."""
-        if self.sets[Set.DRIVER].check_local_inhibitor():
+        if self.network.driver().check_local_inhibitor():
             self.local = 1.0
     
     def fire_local(self):                                               # Fire local inhibitor
         """Fire the local inhibitor."""
-        self.sets[Set.DRIVER].initialise_act(Type.PO)
-        self.sets[Set.RECIPIENT].initialise_act(Type.PO)
-        self.semantics.initialiseSem()
+        self.network.driver().update_ops.init_act([Type.PO])
+        self.network.recipient().update_ops.init_act([Type.PO])
+        self.network.semantics.init_sem()
     
     def check_global(self):                                             # Check global inhibition
         """Check global inhibitor activation."""
-        if self.sets[Set.DRIVER].check_global_inhibitor():
+        if self.network.driver().check_global_inhibitor():
             self.glbal = 1.0
         
     def fire_global(self):                                              # Fire global inhibitor
         """Fire the global inhibitor."""
-        self.sets[Set.DRIVER].initialise_act([Type.PO, Type.RB, Type.P])
-        self.sets[Set.RECIPIENT].initialise_act([Type.PO, Type.RB, Type.P])
-        self.sets[Set.MEMORY].initialise_act([Type.PO, Type.RB, Type.P])
-        self.semantics.initialise_sem()
+        self.network.driver().update_ops.init_act([Type.PO, Type.RB, Type.P])
+        self.network.recipient().update_ops.init_act([Type.PO, Type.RB, Type.P])
+        self.network.memory().update_ops.init_act([Type.PO, Type.RB, Type.P])
+        self.network.semantics.init_sem()
