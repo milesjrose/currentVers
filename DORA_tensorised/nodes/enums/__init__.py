67fe20be5269fa5cbe245b060041e62caaee36b7
milesjrose <s2093547@ed.ac.uk>
2025-09-19 07:29:32 +0100

    update init with new enum

--- DORA_tensorised/nodes/enums/__init__.py
+++ DORA_tensorised/nodes/enums/__init__.py
@@ -1,6 +1,6 @@
 # nodes/enums/__init__.py
 
-from .nodeEnums import SF, TF, MappingFields, Type, Set, Mode, OntStatus, B, null, tensor_type, MAPPING_SETS, feature_type
+from .nodeEnums import SF, TF, MappingFields, Type, Set, Mode, OntStatus, B, Routines, null, tensor_type, MAPPING_SETS, feature_type
 
 __all__ = [
     "SF",
@@ -14,5 +14,6 @@ __all__ = [
     "null",
     "tensor_type",
     "MAPPING_SETS",
-    "feature_type"
+    "feature_type",
+    "Routines"
 ]
