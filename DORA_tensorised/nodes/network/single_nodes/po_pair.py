9457cfa3b1ec57551fb45ec7e58b2155c91dff79
milesjrose <s2093547@ed.ac.uk>
2025-09-19 14:02:32 +0100

    fix po_pair

--- DORA_tensorised/nodes/network/single_nodes/po_pair.py
+++ DORA_tensorised/nodes/network/single_nodes/po_pair.py
@@ -9,7 +9,7 @@ class Pairs(object):
         self.pairs: dict[int, (int, int)] = {}
     
     def hash(self, po1: int, po2: int):
-        if po1>po2:
+        if po1<po2:
             return int(str(po1) + str(po2))
         else:
             return int(str(po2) + str(po1))
@@ -18,11 +18,12 @@ class Pairs(object):
         """
         add a pair of pos
         """
+        self.pairs[self.hash(po1, po2)] = (po1, po2)
     
     def get_list(self) -> list[(int, int)]:
         """
         return list of pairs of indices
         """
-        return self.pairs.items()
+        return list(self.pairs.values())
 
         
