aae068d12e47e100a9365ed9e3aa6837dd0fed65
milesjrose <s2093547@ed.ac.uk>
2025-10-05 19:40:29 +0100

    fix logging plugin not preset

--- DORA_tensorised/nodes/tests/conftest.py
+++ DORA_tensorised/nodes/tests/conftest.py
@@ -11,16 +11,17 @@ def suppress_warnings_filter(record):
 
 def pytest_configure(config):
     logging_plugin = config.pluginmanager.getplugin("logging-plugin")
-    handler = logging_plugin.report_handler
-    
-    # Add %(log_color)s to the beginning of the format string
-    formatter = ShortPathFormatter(
-        fmt='%(log_color)s%(levelname)-8s%(reset)s [%(short_name)s] %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    
-    handler.setFormatter(formatter)
+    if logging_plugin is not None and hasattr(logging_plugin, 'report_handler'):
+        handler = logging_plugin.report_handler
+        
+        # Add %(log_color)s to the beginning of the format string
+        formatter = ShortPathFormatter(
+            fmt='%(log_color)s%(levelname)-8s%(reset)s [%(short_name)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S'
+        )
+        
+        handler.setFormatter(formatter)
 
-    # --- NEW PART: Add the custom filter to the handler ---
-    if suppress_warnings:
-        handler.addFilter(suppress_warnings_filter)
+        # --- NEW PART: Add the custom filter to the handler ---
+        if suppress_warnings:
+            handler.addFilter(suppress_warnings_filter)
