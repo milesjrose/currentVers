3d8aad6a0402a355242c3573eaeebbb526aa793d
miles rose <milespublic@pm.me>
2025-12-05 18:20:48 +0000

    add imports to base_set

--- DORA_tensorised/nodes/network/sets_new/base_set.py
+++ DORA_tensorised/nodes/network/sets_new/base_set.py
@@ -1,25 +1,48 @@
 import torch
+from ..network_params import Params
 from ...enums import *
 from ..tokens.tensor.token_tensor import Token_Tensor
 from ..tokens.tensor_view import TensorView
+from .base_set_ops import TensorOperations, UpdateOperations, AnalogOperations, KludgeyOperations, TokenOperations
+
 class Base_Set:
     """
     Base class for token sets.
     """
-    def __init__(self, tokens: torch.Tensor, token_set: Set):
-        self.tokens: Token_Tensor = tokens
-        self.token_set: Set = token_set
-        self.tensor = self.tokens.get_set_view(self.token_set)
+    def __init__(self, tokens: torch.Tensor, token_set: Set, params: Params):
+        self.glbl: Token_Tensor = tokens
+        """Token_Tensor: Global view of the tensor"""
+        self.tk_set: Set = token_set
+        """Set: Token set"""
+        self.lcl = self.glbl.get_set_view(self.tk_set)
+        """TensorView: Local view of the tensor"""
+        self.params = params
+        """Params: Parameters for the set"""
+        self.tensor_op = TensorOperations(self)
+        self.tnsr = self.tensor_op
+        """TensorOperations: Operations for the tensor"""
+        self.update_op = UpdateOperations(self)
+        self.updt = self.update_op
+        """UpdateOperations: Operations for the update"""
+        self.analog_op = AnalogOperations(self)
+        self.anlg = self.analog_op
+        """AnalogOperations: Operations for the analogs"""
+        self.kludgey_op = KludgeyOperations(self)
+        self.klud = self.kludgey_op
+        """KludgeyOperations: Operations for the kludgey"""
+        self.token_op = TokenOperations(self)
+        self.tkop = self.token_op
+        """TokenOperations: Operations for the tokens"""
     
     def get_tensor(self) -> torch.Tensor:
-        return self.tensor
+        return self.lcl
     
     def get_token_set(self) -> Set:
-        return self.token_set
+        return self.tk_set
     
     def update_view(self) -> TensorView:
-        self.tensor = self.tokens.get_set_view(self.token_set)
-        return self.tensor
+        self.lcl = self.glbl.get_set_view(self.tk_set)
+        return self.lcl
 
     
     
