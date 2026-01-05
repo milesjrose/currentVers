9bbb48457cb57ce64f1bebf2da0477f758814633
milesjrose <s2093547@ed.ac.uk>
2026-01-05 18:42:05 +0000

    start porting over the memory_ops functions

--- DORA_tensorised/nodes/network/operations/memory_ops.py
+++ DORA_tensorised/nodes/network/operations/memory_ops.py
@@ -4,6 +4,7 @@
 from ...enums import *
 from ..sets import Driver, Recipient
 from ..single_nodes import Ref_Analog
+import torch
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ..network import Network
@@ -22,28 +23,21 @@ class TensorOperations:
             network: Reference to the Network object
         """
         self.network: 'Network' = network
-    
-    # --------[ NOTE: I think i ported everything. Idk where these functions went though. ]--------
-
+   
     def make_am(self, copy = True):
         """
         Copy any analogues from memory into AM - based on the set feature.
         Equivalent to make_AM_copy() from helpers.py.
         """
-        analogs = self.network.analog.check_for_copy()
-        for analog in analogs:
-            if copy:
-                self.network.analog.copy(analog, Set.DRIVER)
-            else:
-                self.network.analog.move(analog, Set.DRIVER)
+        raise NotImplementedError("Function is moved to analog_ops.py")
     
     def check_analog_for_tokens_to_copy(self) -> list[Ref_Analog]:
         """Find any analogues with set != memory to move."""
-        analogs = self.network.analog.check_for_copy()
-        return analogs
+        raise NotImplementedError("Function is moved to analog_ops.py")
 
     def del_mem_tokens(self, sets: list[Set] = [Set.DRIVER, Set.RECIPIENT, Set.NEW_SET]):
-        """ delete any memory tokens from the am """
+        """ delete any memory tokens from the am.
+        TODO: Check usage -> there shouldn't be any memory tokens in am after make_am, """
         for set in sets:
             if set not in [Set.DRIVER, Set.RECIPIENT, Set.NEW_SET]:
                 raise ValueError(f"Set {set} is not a valid set for del_mem_tokens")
@@ -51,16 +45,14 @@ class TensorOperations:
 
     def clear_all_sets(self):
         """Clear the set field of every token in memory (i.e. to clear WM)."""
-        for set in Set:
-            if set != Set.MEMORY:
-                self.network.sets[set].token_op.set_features_all(TF.SET, Set.MEMORY)
+        all_idxs = self.network.token_tensor.cache.get_all_nodes_indices()
+        self.network.token_tensor.set_feature(all_idxs, TF.SET, Set.MEMORY)
+        self.network.recache()
     
     def clear_set(self, set: Set):
         """Clear the set of the tokens. (Move to memory set, or delete tokens?)"""
         self.network.sets[set].token_op.set_features_all(TF.SET, Set.MEMORY)
-        # if the tokens are retrieved from memory, delete them, otherwise move them to memory I think?
-        # this gets painful if any retrieved tokens are connected to other tokens in the set.
-        #TODO: Implement this
+        self.network.recache()
     
     def reset_inferences(self):
         """Reset the inferences of all tokens."""
@@ -74,21 +66,14 @@ class TensorOperations:
     
     def swap_driver_recipient(self):
         """Swap the contents of the driver and recipient"""
-        # Create new driver and recipient objects by passsing in opposites tensors
-        old = self.network.recipient()
-        new_nodes = old.nodes.clone()
-        new_nodes[:, TF.SET] = Set.DRIVER
-        new_driver = Driver(new_nodes, old.connections, old.IDs, old.names)
-        old = self.network.driver()
-        new_nodes = old.nodes.clone()
-        new_nodes[:, TF.SET] = Set.RECIPIENT
-        new_rec = Recipient(new_nodes, old.connections, old.IDs, old.names)
-        self.network.sets[Set.DRIVER] = new_driver
-        self.network.sets[Set.RECIPIENT] = new_rec
-        # Update mappings
-        # Just transpose the mapping tensor - idk if we should clear mappings here?
+        # set all driver tokens to recipient set, and vice versa
+        driver_idxs = self.network.token_tensor.cache.get_set_indices(Set.DRIVER)
+        recipient_idxs = self.network.token_tensor.cache.get_set_indices(Set.RECIPIENT)
+        self.network.token_tensor.set_feature(driver_idxs, TF.SET, Set.RECIPIENT)
+        self.network.token_tensor.set_feature(recipient_idxs, TF.SET, Set.DRIVER)
+        # update mappings
         self.network.mappings.swap_driver_recipient()
-        # Update links
-        self.network.links.swap_driver_recipient()
+        # recache
+        self.network.recache()
 
         
