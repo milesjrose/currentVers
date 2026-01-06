9caa0b8a14db1339265b851149d2acf4c2281b3a
miles rose <milespublic@pm.me>
2026-01-06 17:00:31 +0000

    update ctc to run new tests

--- ctc.sh
+++ ctc.sh
@@ -2,7 +2,12 @@
 source .venv/bin/activate 
 
 clear
-
-pytest DORA_tensorised/
-
+echo ">Running unit tests..."
+pytest DORA_tensorised/nodes/tests/unit/
+./clear-cache.sh
+echo ">Running functional tests..."
+pytest DORA_tensorised/nodes/tests/funct/
+./clear-cache.sh
+echo ">Running end-to-end tests..."
+pytest DORA_tensorised/nodes/tests/e2e/
 ./clear-cache.sh
